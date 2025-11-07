251005db56752bbb3a242d4fc32ca9a6ab3adc1d
Lima <limaeternal@gmail.com>
2025-11-07 20:36:12 +0300

    добавление логина и регистрации, фикс багов

--- pages/login.py
+++ pages/login.py
@@ -1,8 +1,20 @@
-from flask import Blueprint, render_template
+# pages/login.py
+from flask import Blueprint, render_template, request, redirect, url_for
+from database.requests import check_password
 
 login_bp = Blueprint("login", __name__)
 
 
-@login_bp.route("/login")
+@login_bp.route("/login", methods=["GET", "POST"])
 def login_page():
+    if request.method == "POST":
+        username = request.form.get("username", "").strip()
+        password = request.form.get("password", "")
+
+        if check_password(username, password):
+            # TODO: установить сессию (пока просто редирект)
+            return redirect(url_for("home.home_page"))
+        else:
+            return render_template("login.html", error="Неверный логин или пароль")
+
     return render_template("login.html")
